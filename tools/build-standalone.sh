80c5a330051399dac928d3bf7303a0f823ede878
Fantaselion <56896802+Fantaselion@users.noreply.github.com>
2022-06-15 15:53:40 -0500

    Update build-standalone.sh

--- tools/build-standalone.sh
+++ tools/build-standalone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir -p build
-gcc $SCRIPT_DIR/../mp2hudcolor/mp2hudcolor.c -o $SCRIPT_DIR/../build/mp2hudcolor
+gcc $SCRIPT_DIR/../PARAMETEREDITOR/Simple_PARAMETER_EDITOR.cpp -o $SCRIPT_DIR/../build/PARAMETEREDITOR
