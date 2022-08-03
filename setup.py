d8a57c82f4c245d8aa69092b73d84ecb4231b71a
unknown <fantaselion@gmail.com>
2022-08-02 22:05:53 -0500

    Test

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     license="MIT",
     packages=['PARAMETEREDITOR'],
     ext_modules=cythonize(PARAMETEREDITOR_module),
-    extra_compile_args=["-std=c++11"],
-    extra_link_args=["-std=c++11"]
+    extra_compile_args=["-std=c++14"],
+    extra_link_args=["-std=c++14"]
 )
