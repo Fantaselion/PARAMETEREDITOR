7b354572c915d197ae7b258863508abe975a9bba
unknown <fantaselion@gmail.com>
2022-08-08 04:42:52 -0500

    Forgot to update version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="PARAMETEREDITOR",
-    version="2.1.2",
+    version="2.2.0",
     description="Randomizes enemy stat values for most of the enemies in the game Metroid Prime.",
     long_description=long_description,
     long_description_content_type="text/markdown",
