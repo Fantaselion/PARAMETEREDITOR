4a0896809f49d40ab622e71725d1a5e13812002f
unknown <fantaselion@gmail.com>
2022-08-11 16:59:50 -0500

    Changed stupid name to a better name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-PARAMETEREDITOR_module = Extension('PARAMETEREDITOR.PARAMETEREDITOR',
-                sources=['PARAMETEREDITOR/PARAMETEREDITOR_wrapper.pyx', 'PARAMETEREDITOR/PARAMETEREDITOR.cpp'],
+Random_Enemy_Attributes_module = Extension('Random_Enemy_Attributes.Random_Enemy_Attributes',
+                sources=['Random_Enemy_Attributes/Random_Enemy_Attributes_wrapper.pyx', 'Random_Enemy_Attributes/Random_Enemy_Attributes.cpp'],
                 extra_compile_args=["-std=c++14"],
                 extra_link_args=["-std=c++14"],
                 language='c++')
@@ -13,15 +13,15 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="PARAMETEREDITOR",
-    version="2.2.1",
+    name="Random_Enemy_Attributes",
+    version="1.0.0",
     description="Randomizes enemy stat values for most of the enemies in the game Metroid Prime.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Fantaselion/PARAMETEREDITOR",
+    url="https://github.com/Fantaselion/Random_Enemy_Attributes",
     author="Fantaselion",
     author_email="fantaselion@gmail.com",
     license="MIT",
-    packages=['PARAMETEREDITOR'],
-    ext_modules=cythonize(PARAMETEREDITOR_module),
+    packages=['Random_Enemy_Attributes'],
+    ext_modules=cythonize(Random_Enemy_Attributes_module),
 )
